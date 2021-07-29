67534a94a7b93e2daa8267da050c8babc6833395
Gaowei Xu <gaowexu@amazon.com>
2021-07-29 12:21:50 +0800

    change to amazonlinux2

--- lambda/recognizer.py
+++ lambda/recognizer.py
@@ -4,9 +4,9 @@ import cv2
 import numpy as np
 
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# for gpu in gpus:
+#     tf.config.experimental.set_memory_growth(gpu, True)
 
 
 class CNLicensePlateRecognizer(object):
